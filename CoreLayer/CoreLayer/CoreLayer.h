ad240c8be3886d9fd60e2ada86b5c9c3d5885431
user149842 <user149842@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-02-23 12:20:14 -0500

    Created projects to separate into layers

---
+++ CoreLayer/CoreLayer/CoreLayer.h
@@ -0,0 +1,19 @@
+//
+//  CoreLayer.h
+//  CoreLayer
+//
+//  Created by user149842 on 2/23/19.
+//  Copyright © 2019 Condor Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreLayer.
+FOUNDATION_EXPORT double CoreLayerVersionNumber;
+
+//! Project version string for CoreLayer.
+FOUNDATION_EXPORT const unsigned char CoreLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreLayer/PublicHeader.h>
+
+
---
+++ DataLayer/DataLayer/DataLayer.h
@@ -0,0 +1,19 @@
+//
+//  DataLayer.h
+//  DataLayer
+//
+//  Created by user149842 on 2/23/19.
+//  Copyright © 2019 Condor Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataLayer.
+FOUNDATION_EXPORT double DataLayerVersionNumber;
+
+//! Project version string for DataLayer.
+FOUNDATION_EXPORT const unsigned char DataLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataLayer/PublicHeader.h>
+
+
